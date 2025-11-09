1a0f0e459bd5266eb34b015482c924ce20bae26f
Сергей Шамбир <sshambir@yandex.ru>
2025-11-09 13:27:49 +0300

    Лексер: добавил список тестов и падающий тест

---
+++ tests/Lexemes.UnitTests/LexerTest.cs
@@ -0,0 +1,39 @@
+namespace Lexemes.UnitTests;
+
+public class LexerTest
+{
+    [Theory]
+    [MemberData(nameof(GetTokenizerIdentifiersAndKeywordsData))]
+    public void Can_tokenize_lexemes(string code, List<Token> expected)
+    {
+        List<Token> actual = Tokenize(code);
+        Assert.Equal(expected, actual);
+    }
+
+    public static TheoryData<string, List<Token>> GetTokenizerIdentifiersAndKeywordsData()
+    {
+        return new TheoryData<string, List<Token>>
+        {
+            {
+                "alice Bob C00L D_2", [
+                    new Token(TokenType.Identifier, "alice"),
+                    new Token(TokenType.Identifier, "Bob"),
+                    new Token(TokenType.Literal, "C00L"),
+                    new Token(TokenType.Identifier, "D_2")
+                ]
+            },
+        };
+    }
+
+    private List<Token> Tokenize(string code)
+    {
+        List<Token> results = [];
+        Lexer lexer = new(code);
+        for (Token t = lexer.ParseToken(); t.Type != TokenType.EndOfFile; t = lexer.ParseToken())
+        {
+            results.Add(t);
+        }
+
+        return results;
+    }
+}
