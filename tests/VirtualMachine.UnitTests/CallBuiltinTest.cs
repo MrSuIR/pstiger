740beeadce8becbbb771cf4861017bce091b6550
Сергей Шамбир <sshambir@yandex.ru>
2026-01-06 13:34:09 +0300

    Виртуальная машина: добавил тесты на остальные встроенные функции, вызываемые с помощью CALL_BUILTIN

--- tests/VirtualMachine.UnitTests/CallBuiltinTest.cs
+++ tests/VirtualMachine.UnitTests/CallBuiltinTest.cs
@@ -7,7 +7,7 @@ public class CallBuiltinTest
 {
     [Theory]
     [MemberData(nameof(GetUseInputAndOutputData))]
-    public void CanUseInputAndOutput(
+    public void Can_use_input_and_output(
         List<Instruction> program,
         string input,
         string expectedBufferedOutput,
@@ -73,6 +73,90 @@ public class CallBuiltinTest
                     new Instruction(InstructionCode.Halt, 0),
                 ],
                 "Input", "In", string.Empty
+            },
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetCallBuiltinFunctionsData))]
+    public void Can_call_builtin_functions(
+        List<Instruction> program,
+        string expectedBufferedOutput
+    )
+    {
+        FakeEnvironment environment = new();
+        TigerVm vm = new(environment, program);
+        Value result = vm.RunProgram();
+
+        Assert.Equal(0, vm.ExitCode);
+        Assert.Equal(Value.Void, result);
+        Assert.Equal(expectedBufferedOutput, environment.BufferedOutput);
+        Assert.Equal(string.Empty, environment.FlushedOutput);
+    }
+
+    public static TheoryData<List<Instruction>, string> GetCallBuiltinFunctionsData()
+    {
+        return new TheoryData<List<Instruction>, string>()
+        {
+            // Функция ord: ord("ABCD") = ord("A") = 65
+            {
+                [
+                    new Instruction(InstructionCode.Push, "ABCD"),
+                    new Instruction(InstructionCode.CallBuiltin, "ord"),
+                    new Instruction(InstructionCode.CallBuiltin, "printi"),
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "65"
+            },
+
+            // Функция chr: chr(40) = "("; chr(41) = ")"
+            {
+                [
+                    new Instruction(InstructionCode.Push, 40),
+                    new Instruction(InstructionCode.CallBuiltin, "chr"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Push, 41),
+                    new Instruction(InstructionCode.CallBuiltin, "chr"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "()"
+            },
+
+            // Функция size: size("Hello, world!") = 13
+            {
+                [
+                    new Instruction(InstructionCode.Push, "Hello, world!"),
+                    new Instruction(InstructionCode.CallBuiltin, "size"),
+                    new Instruction(InstructionCode.CallBuiltin, "printi"),
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "13"
+            },
+
+            // Функция substring: substring("Cogito, ergo sum", 0, 6) = "Cogito"
+            {
+                [
+                    new Instruction(InstructionCode.Push, "Cogito, ergo sum"),
+                    new Instruction(InstructionCode.Push, 0),
+                    new Instruction(InstructionCode.Push, 6),
+                    new Instruction(InstructionCode.CallBuiltin, "substring"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "Cogito"
+            },
+
+            // Функция concat: concat("Cogito", " ergo sum") = "Cogito ergo sum"
+            {
+                [
+                    new Instruction(InstructionCode.Push, "Cogito"),
+                    new Instruction(InstructionCode.Push, " ergo sum"),
+                    new Instruction(InstructionCode.CallBuiltin, "concat"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "Cogito ergo sum"
             },
         };
     }
