285c35e82c898d90ba4bf918c9b9e696ef4fbdf3
Сергей Шамбир <sshambir@yandex.ru>
2026-01-06 21:57:43 +0300

    Виртуальная машина: добавлено ещё два теста на вызов пользовательских функций

--- tests/VirtualMachine.UnitTests/FunctionsTest.cs
+++ tests/VirtualMachine.UnitTests/FunctionsTest.cs
@@ -35,6 +35,8 @@ public class FunctionsTest
                     new Instruction(InstructionCode.Call, 4),
                     new Instruction(InstructionCode.CallBuiltin, "printi"),
                     new Instruction(InstructionCode.Halt, 0),
+
+                    // Начало функции square
                     new Instruction(InstructionCode.StoreVar, "x"),
                     new Instruction(InstructionCode.LoadVar, "x"),
                     new Instruction(InstructionCode.LoadVar, "x"),
@@ -42,6 +44,52 @@ public class FunctionsTest
                     new Instruction(InstructionCode.Return),
                 ],
                 "121"
+            },
+
+            // Можно вызвать процедуру с параметрами
+            {
+                // function printLine(text: str) = (print(text); print("\n"))
+                // printLine("Hello, world!")
+                [
+                    new Instruction(InstructionCode.Push, "Hello, world!"),
+                    new Instruction(InstructionCode.Call, 3),
+                    new Instruction(InstructionCode.Halt, 0),
+
+                    // Начало функции printLine
+                    new Instruction(InstructionCode.StoreVar, "text"),
+                    new Instruction(InstructionCode.LoadVar, "text"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Push, "\n"),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Return),
+                ],
+                "Hello, world!\n"
+            },
+
+            // Можно вызвать функцию для обработки строки с параметрами
+            {
+                // function concat3(a: str, b: str, c: str) = concat(a, concat(b, c))
+                // print(concat3("po", "ta", "to"))
+                [
+                    new Instruction(InstructionCode.Push, "po"),
+                    new Instruction(InstructionCode.Push, "ta"),
+                    new Instruction(InstructionCode.Push, "to"),
+                    new Instruction(InstructionCode.Call, 6),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.Halt, 0),
+
+                    // Начало функции concat3
+                    new Instruction(InstructionCode.StoreVar, "c"),
+                    new Instruction(InstructionCode.StoreVar, "b"),
+                    new Instruction(InstructionCode.StoreVar, "a"),
+                    new Instruction(InstructionCode.LoadVar, "a"),
+                    new Instruction(InstructionCode.LoadVar, "b"),
+                    new Instruction(InstructionCode.LoadVar, "c"),
+                    new Instruction(InstructionCode.CallBuiltin, "concat"),
+                    new Instruction(InstructionCode.CallBuiltin, "concat"),
+                    new Instruction(InstructionCode.Return),
+                ],
+                "potato"
             },
         };
     }
