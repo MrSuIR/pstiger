7bc54a33310688a393fc0bebef5d1ad961c8b535
Сергей Шамбир <sshambir@yandex.ru>
2026-01-06 22:21:14 +0300

    Виртуальная машина: добавлен падающий тест на работу с массивами

---
+++ tests/VirtualMachine.UnitTests/ArrayTest.cs
@@ -0,0 +1,84 @@
+using PsTiger.Runtime;
+using PsTiger.Tests.TestLibrary.TestDoubles;
+
+namespace PsTiger.VirtualMachine.UnitTests;
+
+public class ArrayTest
+{
+    [Theory]
+    [MemberData(nameof(GetUseArraysData))]
+    public void Can_use_arrays(
+        List<Instruction> program,
+        string expectedBufferedOutput
+    )
+    {
+        FakeEnvironment environment = new();
+        TigerVm vm = new(environment, program);
+        Value result = vm.RunProgram();
+
+        Assert.Equal(0, vm.ExitCode);
+        Assert.Equal(Value.Void, result);
+        Assert.Equal(expectedBufferedOutput, environment.BufferedOutput);
+        Assert.Equal(string.Empty, environment.FlushedOutput);
+    }
+
+    public static TheoryData<List<Instruction>, string> GetUseArraysData()
+    {
+        return new TheoryData<List<Instruction>, string>
+        {
+            // Инициализация массива, запись и чтение элементов массива
+            {
+                /*
+                 Код ниже эквивалентен следующей программе:
+                 let
+                   type IntArray = array of int
+                   var x := intArray[4] of 7
+                 in
+                   x[2] := 3
+                   printi(x[0])
+                   for i := 1 to 4 do (
+                     print(",");
+                     printi(x[i])
+                   )
+                 end
+                 */ [
+                    new Instruction(InstructionCode.Push, 4),
+                    new Instruction(InstructionCode.Push, 7),
+                    new Instruction(InstructionCode.CreateArray),
+                    new Instruction(InstructionCode.StoreVar, "x"),
+
+                    // x[2] := 3
+                    new Instruction(InstructionCode.LoadVar, "x"),
+                    new Instruction(InstructionCode.Push, 2),
+                    new Instruction(InstructionCode.Push, 3),
+
+                    // printi(x[0])
+                    new Instruction(InstructionCode.LoadVar, "x"),
+                    new Instruction(InstructionCode.Push, 0),
+                    new Instruction(InstructionCode.LoadArray),
+                    new Instruction(InstructionCode.CallBuiltin, "printi"),
+
+                    // for i := 1 to 4 do ...
+                    new Instruction(InstructionCode.Push, 1),
+                    new Instruction(InstructionCode.StoreVar, "i"),
+                    new Instruction(InstructionCode.LoadVar, "i"),
+                    new Instruction(InstructionCode.Push, 4),
+                    new Instruction(InstructionCode.Less),
+                    new Instruction(InstructionCode.JumpIfFalse, 23),
+
+                    // print(",");
+                    // printi(x[i])
+                    new Instruction(InstructionCode.Push, ","),
+                    new Instruction(InstructionCode.CallBuiltin, "print"),
+                    new Instruction(InstructionCode.LoadVar, "x"),
+                    new Instruction(InstructionCode.LoadVar, "i"),
+                    new Instruction(InstructionCode.LoadArray),
+                    new Instruction(InstructionCode.CallBuiltin, "printi"),
+
+                    new Instruction(InstructionCode.Halt, 0),
+                ],
+                "7, 7, 3, 7"
+            },
+        };
+    }
+}
